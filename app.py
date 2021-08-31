0cfdd6476104a2d1f42ad7b1c2351a5ed79689d6
Lukasz W <lwasowicz90@gmail.com>
2021-08-31 22:41:13 +0200

    Integrate manager and dumper with app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 """Entry file for application"""
 from logging import DEBUG
+import datetime
+
 
 from argparser.argparser import get_input_args
+from dump.dump import Dumper, FileHandler
 from logger import configure_logger
+from provider.manager import ProviderManager
 
 
 if __name__ == '__main__':
     configure_logger(DEBUG)
-    input_args = get_input_args()
+    config_path = get_input_args()['config']
+    provider = ProviderManager(config_path)
+    provider.process()
+
+    filepath = datetime.datetime.today().strftime("data/%Y-%m-%d-%H-%M-%S-headlines.json")
+    Dumper(handler=FileHandler(filepath)).save(provider.headlines)
