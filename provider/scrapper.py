37f4dedec3d024f7ed9e96f40c55b30e0301ab1f
Lukasz W <lwasowicz90@gmail.com>
2021-09-01 07:44:10 +0200

    Fixup: Avoid KeyError in scrapper
    Avoid KeyError where no link is available

--- provider/scrapper.py
+++ provider/scrapper.py
@@ -9,9 +9,9 @@ def scrap_headlines(page_text: str, tag: str, attrs: dict):
     soup = BeautifulSoup(page_text, 'html.parser')
     found = soup.find_all(tag, attrs=attrs)
 
-    return [{'title': item.text, 'link': item['href']} for item in remove_not_valid_entries(found)]
+    return [{'title': item.text, 'link': item.get('href')} for item in remove_not_valid_entries(found)]
 
 
 def remove_not_valid_entries(found_data):
     """Filters out broken items"""
-    return filter(lambda item: item['href'].startswith('https://') and item.text, found_data)
+    return filter(lambda item: item.get('href') and item['href'].startswith('https://') and item.text, found_data)
